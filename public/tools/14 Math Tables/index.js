1f94523076814e61e83cf6d219b79bff929bbd81
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2024-10-25 16:15:15 +0500

    Delete public directory

--- public/tools/14 Math Tables/index.js
+++
@@ -1,9 +0,0 @@
-function table(num){
-    for(var i=1;i<=10;i++){
-        var result = i*num;
-        console.log(`${num} x ${i} = ${result}`);
-    }
-}
-table(5);       // we can write any table here
-
-table(12);
