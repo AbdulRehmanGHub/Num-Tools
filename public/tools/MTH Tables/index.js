ea0ed3a9037613746d476cc7e14d831d2eebe705
abdulrehmanghub <pakkhan9009@gmail.com>
2024-10-27 23:39:59 +0500

    corrected path

--- public/tools/MTH Tables/index.js
+++ public/tools/MTH Tables/index.js
@@ -1,11 +1,15 @@
-let input = document.querySelector("input");
-let result = document.querySelector("#res");
+document.addEventListener("DOMContentLoaded", () => {
+  let input = document.querySelector("input");
+  let result = document.querySelector("#res");
 
-function table() {
-  let inpValue = input.value;
-  result.innerHTML = "";
-  for (let i = 1; i <= 10; i++) {
-    let res = i * inpValue;
-    result.innerHTML += `${inpValue} &nbsp; x &nbsp; ${i} &nbsp; = &nbsp; ${res} <br>`;
+  function table() {
+    let inpValue = input.value;
+    result.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+      let res = i * inpValue;
+      result.innerHTML += `${inpValue} &nbsp; x &nbsp; ${i} &nbsp; = &nbsp; ${res} <br>`;
+    }
   }
-}
+
+  document.querySelector("button").addEventListener("click", table);
+});
